443fb764ea4109f80753cb785cb0ee91a9a44fc2
Carlos Peralta <caperaltasanchez@gmail.com>
2012-04-09 20:52:57 -0500

    test 2

--- roman_numerals_rspec.rb
+++ roman_numerals_rspec.rb
@@ -2,8 +2,15 @@ require 'rspec'
 require File.dirname(__FILE__) + "/roman_numerals.rb"
 
 describe Roman_numerals do 
+	before(:each) do
+		@a = Roman_numerals.new
+	end
+
 	it "Si viene el valor I debe devolver 1" do
-		a = Roman_numerals.new
-		a.rom_to_num("I").should eq(1)
+		@a.rom_to_num("I").should eq(1)
+	end
+
+	it "Si viene el valor II debe devolver 2" do
+		@a.rom_to_num("II").should eq(2)
 	end
 end
